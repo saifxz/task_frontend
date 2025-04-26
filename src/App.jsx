42257f128122da74e98f46b571ff9a47d08148e6
saifxz <saiffuddin9@gmail.com>
2025-04-27 00:45:44 +0530

    solved a bug

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,24 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Tasks from './pages/Tasks';
+import { useEffect, useState } from 'react';
 
 function App() {
-  const token = localStorage.getItem('token');
+  const [token, setToken] = useState(localStorage.getItem('token'));
+
+  useEffect(() => {
+    const handleStorageChange = () => {
+      setToken(localStorage.getItem('token'));
+    };
+
+    window.addEventListener('storage', handleStorageChange);
+    return () => window.removeEventListener('storage', handleStorageChange);
+  }, []);
 
   return (
     <Routes>
       <Route path="/" element={token ? <Tasks /> : <Navigate to="/login" />} />
-      <Route path="/login" element={<Login />} />
+      <Route path="/login" element={<Login setToken={setToken} />} />
       <Route path="/register" element={<Register />} />
     </Routes>
   );
